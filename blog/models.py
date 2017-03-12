e564c826cbda3cb8702f00afe8be0eda4eb27fe4
Juliana Siskova <julka@git.ksp.sk>
2017-03-12 14:12:27 +0100

    update whole model

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,8 @@ from django.utils import timezone
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
-    title = models.TextField()
+    title = models.CharField(max_length=200)
+    text = models.TextField(default="")
     created_date = models.DateTimeField( default=timezone.now )
     published_date = models.DateTimeField( blank=True, null=True )
 
